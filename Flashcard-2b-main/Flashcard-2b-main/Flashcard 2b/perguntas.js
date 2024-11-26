262a43c0bcf4fa4e9fcc7bee66bffbdd8e3dad8d
vitorlindo23 <142257808+vitorlindo23@users.noreply.github.com>
2024-11-26 09:14:54 -0300

    Add files via upload

---
+++ Flashcard-2b-main/Flashcard-2b-main/Flashcard 2b/perguntas.js
@@ -0,0 +1,11 @@
+criaCartao(
+    'Esportes',
+    'Qual o melhor jogador que joga no Brasil?',
+    'De Arrascaeta'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual melhor time do Brasil?',
+    'Flamengo'
+)
